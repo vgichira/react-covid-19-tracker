70c358719d5375f61d66f0e520fb9cc5468ae1a1
Vincent Gichira <vgichira39@gmail.com>
2020-04-30 10:20:20 +0300

    production build

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,10 +8,11 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}})=>{
     if(!confirmed){
         return "Loading..."
     }
-    
+
     return(
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
+                
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
